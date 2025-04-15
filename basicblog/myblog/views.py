1d9d8ce01821c1e6d8a7616893be9be844731da3
Ludwig S <silvaludwig@outlook.com>
2025-04-15 16:58:29 -0300

    styling and creating my own blog post form

--- basicblog/myblog/views.py
+++ basicblog/myblog/views.py
@@ -1,28 +1,28 @@
-from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
-
 from .models import Post
+from .forms import PostForm
 
 
 # def home(request):
 #     return render(request, 'home.html', {})
 
-class HomeView(ListView):
+class HomeView(ListView):  # Home page
     model = Post
     template_name = 'home.html'
 
 
-class ContactView(ListView):
+class ContactView(ListView):  # contact page
     model = Post
     template_name = 'contact.html'
 
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(DetailView):  # blog post detail page
     model = Post
     template_name = "article_details.html"
 
 
-class AddPostView(CreateView):
-    model = Post
-    template_name = 'add_post.html'
-    fields = '__all__'
+class AddPostView(CreateView):  # add blog post form page
+    model = Post  # using post model imported from models/post
+    form_class = PostForm  # using my own post form imported from models/forms
+    template_name = 'add_post.html'  # using the add_post.html file
+    # fields = '__all__'
