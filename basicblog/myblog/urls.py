c9d13bfbce59119368c8d234a201b624fd2b37fc
Ludwig S <silvaludwig@outlook.com>
2025-04-15 12:40:06 -0300

    making article details webpage

--- basicblog/myblog/urls.py
+++ basicblog/myblog/urls.py
@@ -1,6 +1,9 @@
-from . import views
+# from . import views
 from django.urls import path
+from .views import HomeView, ArticleDetailView
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    # path('', views.home, name="home"),
+    path('', HomeView.as_view(), name="home"),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
 ]
